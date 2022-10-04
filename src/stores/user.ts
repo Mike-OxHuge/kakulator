33d940bc8a0ed52a7e18a9bd592a076623913416
Mike-OxHuge <mikelitoris34@icloud.com>
2022-10-04 15:21:09 +0200

    cleanup

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,12 +3,6 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore("storeUser", {
 	state() {
 		return {
-			firstName: "Samuel",
-			midName: "L",
-			lastName: "Jackson",
-			accessToken: "xxxxxxxxxxxxx",
-			counter: 69,
-
 			choices: {
 				includeTwoLayers: false,
 				includeThreeLayers: true,
@@ -27,9 +21,6 @@ export const useUserStore = defineStore("storeUser", {
 		};
 	},
 	actions: {
-		randomizeCounter() {
-			this.counter = Math.random();
-		},
 		setChoices(key: keyof typeof this.choices, value: boolean) {
 			this.choices[key] = value;
 		},
