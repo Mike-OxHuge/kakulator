32116b367535e18aeb63a1fc2410f0e7fbda921b
Mike-OxHuge <mikelitoris34@icloud.com>
2022-10-06 18:33:36 +0200

    firebase init

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,24 @@ import { loadFonts } from "./plugins/webfontloader";
 import * as messages from "./locales";
 import "./registerServiceWorker";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBq297XCSKEJwFMG79bSNypV-ETcz5svks",
+	authDomain: "kakulator.firebaseapp.com",
+	projectId: "kakulator",
+	storageBucket: "kakulator.appspot.com",
+	messagingSenderId: "164329943755",
+	appId: "1:164329943755:web:13d11f2a06c0a9d2d4614e",
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
 const pinia = createPinia();
 pinia.use(piniaPersist);
 const i18n = createI18n({
@@ -18,4 +36,4 @@ const i18n = createI18n({
 });
 loadFonts();
 const app = createApp(App);
-app.use(vuetify).use(pinia).use(i18n).mount("#app");
+app.use(vuetify).use(pinia).use(i18n).use(firebaseapp).mount("#app");
