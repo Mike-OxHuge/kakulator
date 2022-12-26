dc62b8108e20582a5cc9b0cece171d5f4d26b6fd
Mike <mikelitoris34@icloud.com>
2022-12-26 21:03:25 +0100

    stuff

--- src/components/Tutorial/LayersPicker.vue
+++ src/components/Tutorial/LayersPicker.vue
@@ -1,13 +1,14 @@
 <template>
   <v-container class="d-flex flex-column my-auto text-center">
     <p>how many layers do you prefer?</p>
+    // TODO: make it reusable to use in settings after tutorial is done
     <v-row class="py-5">
       <v-col cols="6" v-for="item in Object.keys(userSettings.layers)">
         <v-btn
           block
           color="secondary"
           :variant="userSettings.layers[item] ? 'elevated' : 'outlined'"
-          @click="userSettings.setLayers(item, !!!userSettings.layers[item])"
+          @click="userSettings.setLayers(item, !userSettings.layers[item])"
         >
           {{ item }} layers
         </v-btn>
